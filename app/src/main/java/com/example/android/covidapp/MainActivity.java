c47910308c9ba0e0794b36dd0708271b3c84fb33
Aditya <adikmr567@gmail.com>
2020-04-02 20:21:16 +0530

    Version 1

---
+++ app/src/main/java/com/example/android/covidapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.covidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void citizen(View view){
+        Intent activity2Intent = new Intent(getApplicationContext(), Citizen.class);
+        startActivity(activity2Intent);
+    }
+
+    public void police(View view){
+
+    }
+}
