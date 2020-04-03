245b882f506a7d9369a45928dec7e0937e39c238
Aditya <adikmr567@gmail.com>
2020-04-03 19:33:31 +0530

    branch check

--- app/src/main/java/com/example/android/covidapp/Citizen.java
+++ app/src/main/java/com/example/android/covidapp/Citizen.java
@@ -1,14 +1,88 @@
 package com.example.android.covidapp;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.security.Permissions;
 
 public class Citizen extends AppCompatActivity {
+
+    private Button btnGrant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citizen);
+
+        btnGrant = findViewById(R.id.nearHospital);
+        btnGrant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withActivity(Citizen.this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        startActivity(new Intent(Citizen.this, NearByHospitals.class));
+                        finish();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        if (response.isPermanentlyDenied()) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(Citizen.this);
+                            builder.setTitle("Permission Denied").setMessage("Permission to access device location is permanently denied. You need to go to settings")
+                                    .setNegativeButton("Cancel", null)
+                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent();
+                                            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                            intent.setData(Uri.fromParts("package", getPackageName(), null));
+                                        }
+                                    }).show();
+                        } else {
+                            Toast.makeText(Citizen.this, "Permission Denied", Toast.LENGTH_SHORT);
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+            }
+        });
+    }
+
+    public void nearbyHospital(View view) {
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void call(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:1075"));
+        startActivity(intent);
     }
 }
