efa975492462f5f116048a7540702894dd8659ca
abhijeet <abhijeetvermayash@gmail.com>
2020-04-04 19:30:18 +0530

    complain of person saved to complain database

--- app/src/main/java/com/example/android/covidapp/MainActivity.java
+++ app/src/main/java/com/example/android/covidapp/MainActivity.java
@@ -27,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
 
     }
+    public void volunteer(View view){
+        Intent k=new Intent(getApplicationContext(), Volunteer.class);
+        startActivity(k);
+
+
+    }
 
     public void help(View view)
     {
